7a7137d48b91c08cbf9dd1bc2f48f64d586624d8
Ram <553578+relaxedtomato@users.noreply.github.com>
2024-07-08 16:27:15 -0400

    Update send-email.sh

--- send-email.sh
+++ send-email.sh
@@ -1,8 +1,8 @@
 curl --request POST \
   # Select one of the following lines to uncomment based on your Region (US, EU)
-  # --url https://api.us.nylas.com/v3/grants/<NYLAS_GRANT_ID>/messages/send \
-  # --url https://api.eu.nylas.com/v3/grants/<NYLAS_GRANT_ID>/messages/send \
-  --header 'Authorization: Bearer <NYLAS_API_KEY>' \
+  # --url "https://api.us.nylas.com/v3/grants/$USER_GRANT_ID/messages/send" \
+  # --url "https://api.eu.nylas.com/v3/grants/$USER_GRANT_ID/messages/send" \
+  --header "Authorization: Bearer ${NYLAS_API_KEY}" \
   --header 'Content-Type: application/json' \
   --data '{
 	"subject": "With Love, from Nylas",
